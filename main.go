1002443b1d34f71f68e9a09c4b7f62aaac0e97bc
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-03-21 00:22:12 +0900

    Adding value struct case

--- main.go
+++ main.go
@@ -9,9 +9,13 @@ type Team struct {
 type Challenge struct {
 	Id        string
 	Histories *[]History
+	Logs      []Log
 }
 type History struct {
 	Id string
+}
+type Log struct {
+	Message string
 }
 
 func main() {
@@ -47,11 +51,18 @@ func main() {
 					},
 				}
 			}
+			// newHistories = append(*v.Histories, History{Id: "1"})  // This code cause null pointer panic
+			newLogs := append(v.Logs, Log{
+				Message: "New Message",
+			})
+
 			currentChallenges[i].Histories = &newHistories
+			currentChallenges[i].Logs = newLogs
 		}
 	}
 	team.Challenges = &currentChallenges
 
 	log.Println(team.Id)
 	log.Println("Histories:" + (*(*team.Challenges)[1].Histories)[0].Id)
+	log.Println("Logs:" + (*team.Challenges)[1].Logs[0].Message)
 }
